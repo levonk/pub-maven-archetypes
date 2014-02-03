1a5783ee35339fcd80badf063d4f0331884dcf6e
jing <jing.x.wang@disney.com>
2014-02-03 10:18:11 -0800

    add new modules and spring boot integratin

---
+++ modules/jpa/src/main/resources/archetype-resources/src/main/java/JpaConfiguration.java
@@ -0,0 +1,15 @@
+package ${groupId};
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wangj117 on 1/30/14.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class JpaConfiguration {
+
+}
---
+++ modules/restful-service/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+package ${groupId};
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void testApp(){
+        Assert.assertTrue(true, "dummy test");
+    }
+}
+
