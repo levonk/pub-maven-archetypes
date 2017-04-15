3ab8282879e2fb712ed9953db0b500e61f748314
LevonK <git@lk.oib.com>
2017-04-15 18:16:37 +0000

    add versioncheck script

---
+++ versioncheck.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+MAX_OUTDATED_LIBRARIES=${1:-0}
+
+OUTPUT="target/outdated.txt"
+mkdir -p target
+
+docker run --rm -it \
+	-v ${HOME}/.m2:/home/user/.m2 \
+	-v $(pwd):/workdir levonk/maven:latest \
+	versions:display-dependency-updates \
+	-Dversions.outputFile="$OUTPUT"
+
+OUTDATED_LIBRARIES=`grep . "$OUTPUT" | tail -n +2 | wc -l`
+
+if [ $OUTDATED_LIBRARIES -ge $MAX_OUTDATED_LIBRARIES ]; then
+	echo "There is $OUTDATED_LIBRARIES outdated libraries!\n"
+	printf '%b\n\n' "$(cat target/outdated.txt)"
+	exit $OUTDATED_LIBRARIES
+fi
+
