c5d51d1c9b00c37cf7b48d18f468eef3bca92028
LevonK <git@lk.oib.com>
2017-04-16 02:27:13 +0000

    wip

--- parent-poms/versioncheck.sh
+++ parent-poms/versioncheck.sh
@@ -4,17 +4,21 @@ MAX_OUTDATED_LIBRARIES=${1:-0}
 
 OUTPUT="target/outdated.txt"
 mkdir -p target
+rm ${OUTPUT}
 
 docker run --rm -it \
 	-v ${HOME}/.m2:/home/user/.m2 \
 	-v $(pwd):/workdir levonk/maven:latest \
 	versions:display-dependency-updates \
+	dependency:tree \
 	-Dversions.outputFile="${OUTPUT}"
 
 OUTDATED_LIBRARIES=`grep . "${OUTPUT}" | tail -n +2 | wc -l`
 
 if [ ${OUTDATED_LIBRARIES} -ge ${MAX_OUTDATED_LIBRARIES} ]; then
 	echo "There is ${OUTDATED_LIBRARIES} outdated libraries!\n"
+	echo "Hit [ENTER] to see summary\n"
+	read FOO
 	printf '%b\n\n' "$(cat ${OUTPUT})"
 	exit ${OUTDATED_LIBRARIES}
 fi
