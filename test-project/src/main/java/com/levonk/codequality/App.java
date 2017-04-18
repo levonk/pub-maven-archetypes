119760f550840b125a1aca1b3a8e9162cd703857
LevonK <git@lk.oib.com>
2017-04-18 00:37:21 +0000

    wip

--- test-project/src/main/java/com/levonk/codequality/App.java
+++ test-project/src/main/java/com/levonk/codequality/App.java
@@ -2,12 +2,20 @@ package com.levonk.codequality;
 
 /**
  * Hello world.
- *
+ * @author LevonK
+ * @version 0.1
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public final class App {
+	/** Private ctor. */
+	private App() {
+		throw new UnsupportedOperationException();
+	}
+
+	/** Test Method.
+	 * @param args Command Line Args
+	 * */
+	public static void main( final String[] args ) {
+		System.out.println( "Hello World!" );
+	}
 }
+
